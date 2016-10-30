f04fad2bc22159bf4f6e78e62df158752c471787
Jamil Hallal <jamilhallal@hotmail.com>
2016-10-30 10:31:40 +0200

    Add files via upload

--- zender-test.php
+++ zender-test.php
@@ -5,13 +5,13 @@
  <body>
 <?php include("zender-php.php"); ?>
 <?php 
-    $message = new ZenderMessage('YOUR_ZENDER_API_KEY');
+	$message = new ZenderMessage('YOUR_ZENDER_API_KEY');
     $message->set_from("from@example.com");
     $message->set_recipient("to@example.com");
     $message->set_fromName("Stefan Mischook");
     $message->set_subject("Hello World from the Zender PHP Library!");
     $message->set_body("<b>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</b>");
-    $message->set_isBodyHtml("false");
+    $message->set_isBodyHtml("true");
     $message->sendMail();
 ?>
  </body>
